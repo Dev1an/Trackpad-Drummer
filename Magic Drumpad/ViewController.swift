a8ada4dbad348cd29ceb1e92c5e22304cb0fa845
Damiaan Dufaux <damiaan@dpro.be>
2017-06-18 14:45:43 +0200

    Initial Commit

---
+++ Magic Drumpad/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Magic Drumpad
+//
+//  Created by Damiaan on 18/06/17.
+//  Copyright © 2017 Damiaan Dufaux. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
